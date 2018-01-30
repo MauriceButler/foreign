5a2389ab304bc8399c80a6b3cf1876cfffb4ecfc
Maurice Butler <maurice.butler@gmail.com>
2018-01-30 13:54:43 +1000

    cleanup and updates

--- tests/index.js
+++ tests/index.js
@@ -3,48 +3,51 @@ var test = require('tape'),
     majigger = 0;
 
 
-function processItems(item, callback){
-    if(item === undefined){
+function processItems(item, callback) {
+    if (item === undefined) {
         throw new Error('item is undefined');
     }
 
-    if(item !== 3){
+    if (item !== 3) {
         return callback(null, majigger++);
     }
 
-    setTimeout(function(){
+    setTimeout(function () {
         callback(null, majigger++);
     }, 500);
 }
 
 
-test('parallel with array', function(t){
+test('parallel with array', function (t) {
     t.plan(2);
 
     foreign.parallel(
         processItems,
-        [1,2,3,4],
-        function(error, results){
+        [1, 2, 3, 4],
+        function (error, results) {
             t.notOk(error, 'no error');
-            t.ok(results[0] < results[1] && results[1] < results[2] && results[3] < results[2], 'Correct order of completion');
+            t.ok(
+                results[0] < results[1] && results[1] < results[2] && results[3] < results[2],
+                'Correct order of completion'
+            );
         }
     );
 });
 
-test('parallel handles empty array', function(t){
+test('parallel handles empty array', function (t) {
     t.plan(2);
 
     foreign.parallel(
         processItems,
         [],
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.deepEqual(results, [], 'Handles empty array');
         }
     );
 });
 
-test('parallel with object', function(t){
+test('parallel with object', function (t) {
     t.plan(2);
 
     foreign.parallel(
@@ -53,55 +56,61 @@ test('parallel with object', function(t){
             foo: 1,
             bar: 2,
             meh: 3,
-            stuff:4
+            stuff: 4,
         },
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
-            t.ok(results.foo < results.bar && results.bar < results.meh && results.stuff < results.meh, 'Correct order of completion');
+            t.ok(
+                results.foo < results.bar && results.bar < results.meh && results.stuff < results.meh,
+                'Correct order of completion'
+            );
         }
     );
 });
 
-test('parallel handles object with no keys', function(t){
+test('parallel handles object with no keys', function (t) {
     t.plan(2);
 
     foreign.parallel(
         processItems,
         {},
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.deepEqual(results, {}, 'Handles object with no keys');
         }
     );
 });
 
-test('series with array', function(t){
+test('series with array', function (t) {
     t.plan(2);
 
     foreign.series(
         processItems,
-        [1,2,3,4],
-        function(error, results){
+        [1, 2, 3, 4],
+        function (error, results) {
             t.notOk(error, 'no error');
-            t.ok(results[0] < results[1] && results[1] < results[2] && results[2] < results[3], 'Correct order of completion');
+            t.ok(
+                results[0] < results[1] && results[1] < results[2] && results[2] < results[3],
+                'Correct order of completion'
+            );
         }
     );
 });
 
-test('series handles empty array', function(t){
+test('series handles empty array', function (t) {
     t.plan(2);
 
     foreign.series(
         processItems,
         [],
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.deepEqual(results, [], 'Handles empty array');
         }
     );
 });
 
-test('series with object', function(t){
+test('series with object', function (t) {
     t.plan(2);
 
     foreign.series(
@@ -110,99 +119,102 @@ test('series with object', function(t){
             foo: 1,
             bar: 2,
             meh: 3,
-            stuff:4
+            stuff: 4,
         },
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
-            t.ok(results.foo < results.bar && results.bar < results.meh && results.meh < results.stuff, 'Correct order of completion');
+            t.ok(
+                results.foo < results.bar && results.bar < results.meh && results.meh < results.stuff,
+                'Correct order of completion'
+            );
         }
     );
 });
 
-test('series handles object with no keys', function(t){
+test('series handles object with no keys', function (t) {
     t.plan(2);
 
     foreign.series(
         processItems,
         {},
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.deepEqual(results, {}, 'Handles object with no keys');
         }
     );
 });
 
-test('parallel with array with extra keys', function(t){
+test('parallel with array with extra keys', function (t) {
     t.plan(2);
 
-    var items = [1,2,3,4];
+    var items = [1, 2, 3, 4];
 
     items.foo = 'bar';
 
     foreign.parallel(
         processItems,
         items,
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.ok(results.length === 4, 'Correct number of results');
         }
     );
 });
 
-test('series with array with extra keys', function(t){
+test('series with array with extra keys', function (t) {
     t.plan(2);
 
-    var items = [1,2,3,4];
+    var items = [1, 2, 3, 4];
 
     items.foo = 'bar';
 
     foreign.parallel(
         processItems,
         items,
-        function(error, results){
+        function (error, results) {
             t.notOk(error, 'no error');
             t.ok(results.length === 4, 'Correct number of results');
         }
     );
 });
 
-test('parallel error on last item wont callback twice', function(t){
+test('parallel error on last item wont callback twice', function (t) {
     t.plan(2);
 
-    var items = [1,2,3,4];
+    var items = [1, 2, 3, 4];
 
     foreign.parallel(
-        function(item, callback){
-            if(item === 4){
+        function (item, callback) {
+            if (item === 4) {
                 return callback('BANG');
             }
 
             callback(null, item);
         },
         items,
-        function(error, results){
+        function (error, results) {
             t.equal(error, 'BANG', 'correct error');
             t.notOk(results, 'no results');
         }
     );
 });
 
-test('handels additional arguments', function(t){
+test('handels additional arguments', function (t) {
     t.plan(2);
 
     foreign.series(
-        function(item, callback){
+        function (item, callback) {
             callback(null, item, 'foo');
         },
-        [1,2,3],
-        function(error, results){
+        [1, 2, 3],
+        function (error, results) {
             t.notOk(error, 'no error');
             t.deepEqual(results, [[1, 'foo'], [2, 'foo'], [3, 'foo']], 'correct result');
         }
     );
 });
 
-test('series with massive stack', function(t){
+test('series with massive stack', function (t) {
     t.plan(2);
 
     var data = [];
@@ -212,11 +224,11 @@ test('series with massive stack', function(t){
     }
 
     foreign.series(
-        function(item, callback){
+        function (item, callback) {
             callback(null, item);
         },
         data,
-        function(error, result){
+        function (error, result) {
             t.notOk(error, 'no error');
             t.equal(50000, result.length, 'didnt explode');
         }
